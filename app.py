69a507a9610a059cc67c15ba8b5d61916ec1910e
Ankit Jindal <jindalankit940@gmail.com>
2025-07-16 06:16:43 +0530

    Added code

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # app.py
+
+"""
+This module defines a simple Flask web application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+
+    """
+    This function returns a greeting message when the root URL is accessed.
+    """
+
+    
     return 'Hello, CI/CD!'
 
 if __name__ == '__main__':
---
+++ test_app.py
@@ -0,0 +1,14 @@
+# test_app.py
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    """Test the / route"""
+    response = client.get('/')
+    assert response.data == b'Hello, CI/CD!'
+    assert response.status_code == 200
